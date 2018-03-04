af9c224ee5689249ca906d5efbb3d43b01a4b683
杨剑锋 <331387202@qq.com>
2018-03-04 23:22:30 +0800

    Update settings

--- weibo_users/settings.py
+++ weibo_users/settings.py
@@ -18,7 +18,7 @@ MONGO_URI = 'localhost'
 MONGO_DATABASE = 'weibo'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
